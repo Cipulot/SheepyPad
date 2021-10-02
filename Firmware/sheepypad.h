29814d16079934f078d190f91e8e329d1ab62487
Cipulot <40441626+Cipulot@users.noreply.github.com>
2021-10-02 09:55:01 +0200

    Various changes
    info.json: Modified keymap layout for proper VIA recognition.
    sheepypad.h: modified LAYOUT to reflect matrix wiring and blanks.
    keymap.c: fixed keymap and added primitive calculator features

--- Firmware/sheepypad.h
+++ Firmware/sheepypad.h
@@ -5,16 +5,17 @@
 #define xxx KC_NO
 
 #define LAYOUT( \
-	K00, K01, K02, K03, K04, \
+	K00,                     \
 	     K11, K12, K13, K14, \
 	     K21, K22, K23, K24, \
 	     K31, K32, K33, K34, \
-	     K41, K42, K43, K44  \
+	     K41, K42, K43, K44, \
+	     K51, K52, K53, K54  \
 ) { \
-	{ K00,   K01,   K02,   K03,   K04 }, \
+	{ K00,   xxx,   xxx,   xxx,   xxx }, \
 	{ xxx,   K11,   K12,   K13,   K14 }, \
 	{ xxx,   K21,   K22,   K23,   K24 }, \
 	{ xxx,   K31,   K32,   K33,   K34 }, \
 	{ xxx,   K41,   K42,   K43,   K44 }, \
-	{ xxx,   xxx,   xxx,   xxx,   xxx }  \
+	{ xxx,   K51,   K52,   K53,   K54 }  \
 }
